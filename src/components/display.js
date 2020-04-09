c7a612b7301a0a47f74425ac6fcd6bd8f2ce8b75
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-09 11:30:06 -0400

    Update display.js
    Converted Display function to ES6 function and imbedded value prop in argument using object destructuring

--- src/components/display.js
+++ src/components/display.js
@@ -2,16 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import logo from '../css/logo.svg';
 
-function Display(props) {
-  const { value } = props;
-
+const Display = ({ value }) => {
   return (
     <div className="display">
       {value}
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
-}
+};
 
 // Specifies the default values for props:
 Display.defaultProps = {
