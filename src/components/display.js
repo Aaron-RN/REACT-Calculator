6b142969fc3b7796724977536a982c0f170aca6f
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-09 11:37:54 -0400

    Linter Fix

--- src/components/display.js
+++ src/components/display.js
@@ -2,14 +2,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import logo from '../css/logo.svg';
 
-const Display = ({ value }) => {
-  return (
-    <div className="display">
-      {value}
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
-};
+const Display = ({ value }) => (
+  <div className="display">
+    {value}
+    <img src={logo} className="App-logo" alt="logo" />
+  </div>
+);
 
 // Specifies the default values for props:
 Display.defaultProps = {
