8ee0f071955cfcbc65cc1e817652361c34074526
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-04-09 17:00:16 -0400

    merged changes from m4 into m5 branch

--- src/components/display.js
+++ src/components/display.js
@@ -2,17 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import logo from '../css/logo.svg';
 
-function Display(props) {
-  const { value, history } = props;
-
-  return (
-    <div className="display">
-      <div className="history">{history}</div>
-      {value || '0'}
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
-}
+const Display = ({ value, history }) => (
+  <div className="display">
+    <div className="history">{history}</div>
+    {value || '0'}
+    <img src={logo} className="App-logo" alt="logo" />
+  </div>
+);
 
 // Specifies the default values for props:
 Display.defaultProps = {
